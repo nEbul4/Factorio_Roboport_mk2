1535a85df2761af94e08bd5ce836e6846cd65102
Mika Busch <haeki@hotmail.de>
2021-03-04 00:57:02 +0100

    fixed images not working

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -37,7 +37,40 @@ roboportmk2.charging_offsets = {
 {-1.5,-1.5},									   			                         {1.5,-1.5},
 {-1.5,-1.5},{-1.0,-1.5},{-0.5,-1.5},{0.0,-1.5},{0.5,-1.5},{1.0,-1.5},{1.5,-1.5}
 }
-
+roboportmk2.base.layers =
+  {
+    {
+      filename = "__Factorio_Roboport_mk2__/graphics/entity/roboport/roboport-mk2-base.png",
+      width = 143,
+      height = 135,
+      shift = {0.5, 0.25},
+      hr_version =
+      {
+        filename = "__Factorio_Roboport_mk2__/graphics/entity/roboport/hr-roboport-mk2-base.png",
+        width = 228,
+        height = 277,
+        shift = util.by_pixel(2, 7.75),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__base__/graphics/entity/roboport/roboport-shadow.png",
+      width = 147,
+      height = 101,
+      draw_as_shadow = true,
+      shift = util.by_pixel(28.5, 19.25),
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/roboport/hr-roboport-shadow.png",
+        width = 294,
+        height = 201,
+        draw_as_shadow = true,
+        force_hr_shadow = true,
+        shift = util.by_pixel(28.5, 19.25),
+        scale = 0.5
+      }
+    }
+  }
 roboportmk3 = deepCopy(data.raw.roboport["roboport"])
 roboportmk3.name = "roboport-mk3"
 roboportmk3.fast_replaceable_group = "roboport"
@@ -64,7 +97,40 @@ roboportmk3.charging_offsets = {
 {-1.5,-1.5},									   			                          {1.5,-1.5},
 {-1.5,-1.5},{-1.0,-1.5},{-0.5,-1.5},{0.0,-1.5},{0.5,-1.5},{1.0,-1.5},{1.5,-1.5}
 }
-
+roboportmk3.base.layers =
+  {
+    {
+      filename = "__Factorio_Roboport_mk2__/graphics/entity/roboport/roboport-mk3-base.png",
+      width = 143,
+      height = 135,
+      shift = {0.5, 0.25},
+      hr_version =
+      {
+        filename = "__Factorio_Roboport_mk2__/graphics/entity/roboport/hr-roboport-mk3-base.png",
+        width = 228,
+        height = 277,
+        shift = util.by_pixel(2, 7.75),
+        scale = 0.5
+      }
+    },
+    {
+      filename = "__base__/graphics/entity/roboport/roboport-shadow.png",
+      width = 147,
+      height = 101,
+      draw_as_shadow = true,
+      shift = util.by_pixel(28.5, 19.25),
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/roboport/hr-roboport-shadow.png",
+        width = 294,
+        height = 201,
+        draw_as_shadow = true,
+        force_hr_shadow = true,
+        shift = util.by_pixel(28.5, 19.25),
+        scale = 0.5
+      }
+    }
+  }
 
 data:extend(
   {
